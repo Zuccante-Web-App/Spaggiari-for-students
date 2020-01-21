34093a222d6db16494bb1f000df014fb21868bbb
Filippo Niero <34870325+iHeroN0@users.noreply.github.com>
2020-01-21 15:00:36 +0100

    Added serialization to LoginResponse class

--- classemorta/lib/Service/Network/Responses/LoginResponse.dart
+++ classemorta/lib/Service/Network/Responses/LoginResponse.dart
@@ -5,4 +5,24 @@ class LoginResponse {
     String _token;
     String _relase;
     String _expire;
+  
+    LoginResponse.fromJson(Map<String, dynamic> json):
+      _ident = json["ident"], 
+      _firstName = json["firstName"], 
+      _lastName = json["lastName"], 
+      _token = json["token"], 
+      _relase = json["relase"], 
+      _expire = json["expire"];
+      
+  Map<String, dynamic> toJson() =>
+    {
+      
+      'ident': _ident,
+      'firstName': _firstName,
+      'lastName': _lastName,
+      'token': _token,
+      'relase': _relase,
+      'expire': _expire,
+    };
+
 }
